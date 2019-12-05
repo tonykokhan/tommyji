51986337f41d417c887b973f2a0930906c4cd9b7
tonykokhan <tommykokhan@gmail.com>
2019-12-05 17:36:38 +0800

    create exception_handling

---
+++ exception_handling.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+__author__ = 'tommyji'
+__date__ = '2019/12/5 15:52'
+
+# i = j
+
+# print())
+
+# a = '123'
+# print(a[3])
+
+# d = {'a': 1, 'b': 2}
+# print(d['a'])
+# print(d['c'])
+
+year = int(input('input year:'))
+
+try:
+    year = int(input('input year:'))
+except ValueError:
