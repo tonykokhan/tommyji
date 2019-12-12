1b10c684ff85b4e5641db5af3758c34a83bb5efc
tonykokhan <tommykokhan@gmail.com>
2019-12-12 15:07:27 +0800

    create time_test.py

---
+++ time_test.py
@@ -0,0 +1,15 @@
+import time
+import datetime
+# print(time.time())
+# print(time.localtime())
+# print(time.strftime('%Y-%m-%d %H:%M:%S'))
+# print(time.strftime('%Y%m%d'))
+
+
+print(datetime.datetime.now())
+newtime = datetime.timedelta(minutes=10)
+print(datetime.datetime.now() + newtime)
+
+one_day = datetime.datetime(2008, 5, 27)
+new_date = datetime.timedelta(days=10)
+print(one_day + new_date)
