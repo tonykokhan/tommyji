213ea0c7784380788781cbc42f82b9417c57ac68
tonykokhan <tommykokhan@gmail.com>
2019-12-17 18:08:51 +0800

    update pachong

--- pachong7.py
+++ pachong7.py
@@ -13,7 +13,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER"
 }
 
-# url = 'http://www.infoq.com/cn/news'
+# url = 'http://www.infoq.com/cn/news'      # 链接已失效
 url = 'http://www.infoq.com/news'
 
 
