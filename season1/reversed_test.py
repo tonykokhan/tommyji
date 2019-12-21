b1d86a21b62ee8b54b2119da858b996eb1ef7658
tonykokhan <tommykokhan@gmail.com>
2019-12-22 00:19:26 +0800

    update reversed

--- season1/reversed_test.py
+++ season1/reversed_test.py
@@ -5,6 +5,7 @@ a = [20, 10, 30, 40]
 c = reversed(a)
 print(c)
 print(list(c))
+print(list(c))
 
 # 我们打印输出c发现提示：list_reverseiterator。也就是一个迭代对象。同时，我们使用list(c)进行输出，发现只能使用一次。
 # 第一次输出了元素，第二次为空。那是因为迭代对象在第一次时已经遍历结束了，第二次不能再使用。
