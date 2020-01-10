bd53efeb47adcf01e366d00d410ee88db587558e
tonykokhan <tommykokhan@gmail.com>
2020-01-10 18:20:26 +0800

    特殊方法和运算符重载

---
+++ season1/mypro_obj/myoo_21.py
@@ -0,0 +1,32 @@
+# 测试运算符的重载
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __add__(self, other):
+        if isinstance(other, Person):
+            print(other)
+            print(Person)
+            # print(self.name)
+            # print(other.name)
+            return "{0}--{1}".format(self.name, other.name)
+        else:
+            return "不是同类对象，不能相加"
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            print(other)
+            return self.name * other
+        else:
+            return "不是同类对象，不能相乘"
+
+
+p1 = Person("计开源")
+p2 = Person("计小开")
+
+# p1 + p2   # 调用__add__()方法
+x = p1 + p2
+print(x)    # 打印函数的返回值
+# print(x * 3)
+# print(p1 * 3)
+print(p2 * 3)
