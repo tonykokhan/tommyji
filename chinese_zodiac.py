b7c3fff851cab36fada8b646e9d70444dd463ab5
tonykokhan <tommykokhan@gmail.com>
2020-09-16 17:44:32 +0800

    aaa

--- chinese_zodiac.py
+++ chinese_zodiac.py
@@ -2,24 +2,14 @@
 
 chinese_zodiac = '猴鸡狗猪鼠牛虎兔龙蛇马羊'
 
-
 # print(chinese_zodiac[0])
-
 # print(chinese_zodiac[0:4])
-
 # print(chinese_zodiac[-1])
 
 year = 2019
 print(year % 12)
-
 print(chinese_zodiac[year % 12])
-
 print('狗' not in chinese_zodiac)
-
+print('狗' in chinese_zodiac)
 print(chinese_zodiac + 'abcd')
-
 print(chinese_zodiac * 3)
-
-
-
-
--- chinese_zodiac_v2.py
+++ chinese_zodiac_v2.py
@@ -2,30 +2,30 @@
 
 chinese_zodiac = '猴鸡狗猪鼠牛虎兔龙蛇马羊'
 
-# # year = 2018
-# year = int(input('请用户输入出生年份：'))
-#
-# # print(chinese_zodiac[year % 12])
-#
-# if(chinese_zodiac[year % 12]) == '狗':
-#     print('狗年运势。。。')
+# year = 2018
+year = int(input('请用户输入出生年份：'))
+
+# print(chinese_zodiac[year % 12])
+
+if(chinese_zodiac[year % 12]) == '狗':
+    print('狗年运势。。。')
 
 # for cz in chinese_zodiac:
 #     print(cz)
 #
 # for i in range(1, 13):
 #     print(i)
-#
-# for year in range(2000, 2069):
-#     print('%s 年的生肖是 %s' % (year, chinese_zodiac[year % 12]))
+
+for year in range(2000, 2069):
+    print('%s 年的生肖是 %s' % (year, chinese_zodiac[year % 12]))
+
 # import time
-num = 5
-while True:
-    num = num + 1
-
-    if num == 10:
-        continue    # 跳过当前这次循环
-
-    print(num)
-    # time.sleep(1)
-
+# num = 5
+# while True:
+#     num = num + 1
+#
+#     if num == 10:
+#         continue    # 跳过当前这次循环
+#
+#     print(num)
+#     # time.sleep(1)
