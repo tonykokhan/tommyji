eb7410d33449d3e345cbaef77d49826d4a39afec
tonykokhan <tommykokhan@gmail.com>
2020-09-23 23:37:49 +0800

    aaa

--- python_devops_and_best_practices/difflib/simple3.py
+++ python_devops_and_best_practices/difflib/simple3.py
@@ -16,7 +16,9 @@ except Exception as e:
 def readfile(filename):  # 文件读取分隔函数
     try:
         fileHandle = open(filename, 'rb')
-        text = fileHandle.read().splitlines()  # 读取后以行进行分隔
+        string = fileHandle.read().decode()    # python3为字节，python2为字符串。需要将字节转换为字符串，参考：https://blog.csdn.net/cunchi4221/article/details/107475824
+        text = string.splitlines()  # 读取字符串后以行进行分隔，然后放到一个列表里
+        print(text)
         fileHandle.close()
         return text
     except IOError as error:
@@ -29,10 +31,10 @@ if textfile1 == "" or textfile2 == "":
     sys.exit()
 
 text1_lines = readfile(textfile1)			# 调用readfile函数，获取分隔后的字符串
-print(text1_lines)
+# print(text1_lines)
 text2_lines = readfile(textfile2)
 # print(text2_lines)
 
 d = difflib.HtmlDiff()				# 创建HtmlDiff()类对象
-# print(d.make_file(text1_lines, text2_lines))
+print(d.make_file(text1_lines, text2_lines))
 # print(d.make_file(text1_lines, text2_lines))  # 通过make_file方法输出HTML格式的比对结果
